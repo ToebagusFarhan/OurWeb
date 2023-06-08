87ee285e5f41117360590d3d8fe4042a3c33ce23
Tubagus Farhan <3337220038@untirta.ac.id>
2023-06-08 20:16:44 +0700

    Rewrite All Code

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
 
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleBtn = document.querySelector('.toggle_btn');
-    const toggleBtnIcon = document.querySelector('.toggle_btn i');
-    const toggleBtnMenu = document.querySelector('.dropdown_menu');
-  
-    toggleBtn.onclick = function () {
-      toggleBtnMenu.classList.toggle('open');
-      const isOpen = toggleBtnMenu.classList.contains('open');
-  
-      toggleBtnIcon.className = isOpen 
-        ? 'fa fa-xmark' 
-        : 'fa fa-bars';
-    };
-  });
-  
+menuToggle.addEventListener('click', function() {
+    nav.classList.toggle('slide');
+});
