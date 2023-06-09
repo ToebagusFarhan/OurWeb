17fec2b9155a769fc8ba7236c3609bd1a0a96122
Tubagus Farhan <3337220038@untirta.ac.id>
2023-06-09 14:40:04 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+
+// Burger button
 const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
+});
+
+//Saat klik navbar akan smooth
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
